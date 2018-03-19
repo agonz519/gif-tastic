bf2e6111fd89c254579787978040c087b17e0f57
Alexander <agonz519@gmail.com>
2018-03-18 21:21:54 -0400

    Added user input creating new buttons. Main functionality complete.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,47 +5,54 @@ var btns = main.find("#movieButtons");
 
 
 //FOR LOOP TO CREATE BUTTONS FOR MOVIES==================================
-for (var i = 0; i < movies.length; i++) {
-	var movieBtn = $("<button>");
-	movieBtn.addClass("movie-button movie movie-button-color");
-	movieBtn.attr("data-movie", movies[i]);
-	movieBtn.text(movies[i]);
-	btns.append(movieBtn);
+function createButtons() {
+	for (var i = 0; i < movies.length; i++) {
+		var movieBtn = $("<button>");
+		movieBtn.addClass("movie-button movie movie-button-color");
+		movieBtn.attr("data-movie", movies[i]);
+		movieBtn.text(movies[i]);
+		btns.append(movieBtn);
+	}
 } //=====================================================================
 
-//BUTTON CLICKS & AJAX CALL TO GIPHY API & IMG CREATION=================
-$("button").on("click", function () {
-	var movie = $(this).attr("data-movie");
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=bbfAc7Nt5KQyvpe6eD6xsvJkQWPhauAV&limit=10";
+//BUTTON CLICKS & AJAX CALL TO GIPHY API & IMG CREATION==================
+function ajaxCall() {
+	$("button").on("click", function () {
+		$("#gifs-appear-here").empty();
+		var movie = $(this).attr("data-movie");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=bbfAc7Nt5KQyvpe6eD6xsvJkQWPhauAV&limit=10";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).then(function (response) {
-		console.log(response);
-		var results = response.data;
-		for (var i = 0; i < results.length; i++) {
-			var gifDiv = $("<div class='item'>");
-			var rating = results[i].rating;
-			var p = $("<p>").text("Rating: " + rating);
-			var movieImage = $("<img>");
-			movieImage.addClass("gif");
-			movieImage.attr("src", results[i].images.fixed_height_still.url);
-			movieImage.attr("data-still", results[i].images.fixed_height_still.url);
-			movieImage.attr("data-animate", results[i].images.fixed_height.url);
-			movieImage.attr("data-state", "still");
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).then(function (response) {
+			console.log(response);
+			var results = response.data;
+			for (var i = 0; i < results.length; i++) {
+				var gifDiv = $("<div class='item'>");
+				var rating = results[i].rating;
+				var p = $("<p>").text("Rating: " + rating);
+				var movieImage = $("<img>");
+				movieImage.addClass("gif");
+				movieImage.attr("src", results[i].images.fixed_height_still.url);
+				movieImage.attr("data-still", results[i].images.fixed_height_still.url);
+				movieImage.attr("data-animate", results[i].images.fixed_height.url);
+				movieImage.attr("data-state", "still");
 
-			gifDiv.prepend(p);
-			gifDiv.prepend(movieImage);
+				gifDiv.prepend(p);
+				gifDiv.prepend(movieImage);
 
-			$("#gifs-appear-here").prepend(gifDiv);
-		}
+				$("#gifs-appear-here").prepend(gifDiv);
+			}
+		});
 	});
-}); //===================================================================
+} //====================================================================
 
-//ANIMATE GIFS WHEN CLICKING ON THEM=======================================
-$(".gif").on("click", function () {
-	alert("You clicked me!");
+createButtons();
+ajaxCall();
+
+//ANIMATE GIFS WHEN CLICKING ON THEM======================================
+$(document).on("click", ".gif", function () {
 	var state = $(this).attr("data-state");
 	if (state === "still") {
 		var animatedUrl = $(this).attr("data-animate");
@@ -56,5 +63,15 @@ $(".gif").on("click", function () {
 		$(this).attr("src", stillUrl);
 		$(this).attr("data-state", "still");
 	}
-}); //===================================================================
+}); //====================================================================
 
+//TAKE USER INPUT AND ADD A NEW BUTTON TO THE TOP=========================
+$("#addMovie").on("click", function(event) {
+	event.preventDefault();
+	var newMovie = $("#movie-input").val();
+	movies.push(newMovie);
+	$("#movie-form").trigger("reset");
+	btns.empty();
+	createButtons();
+	ajaxCall();
+});
